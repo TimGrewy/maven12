0e91b838c8e9eb1979800db6ad49eef08cf63fd6
Tim Grewy <grewy@hotmail.com>
2014-08-27 22:07:41 +0200

    Dynamic resovle of URL for plunker edits vs production

--- maven12/src/main/webapp/script.js
+++ maven12/src/main/webapp/script.js
@@ -2,29 +2,35 @@
 
 var app = angular.module("chat", ['luegg.directives']);
 
-var MainController = function($scope, $http, $interval) {
-    $http.defaults.useXDomain = true;
-    var baseUrl = 'http://localhost:8080';
-    var baseUrl = 'http://grewy.dk';
-    $scope.pageLoads = 0;
-   
-    $scope.getAllMessages = function($scope, $http) {
+var MainController = function($scope, $http, $interval, $location) {
+  $http.defaults.useXDomain = true;
+  var baseUrl = 'http://grewy.dk/chatster';
+  if ($location.host() == 'run.plnkr.co') {
+    baseUrl = 'http://localhost:8080/chatster';
+  }
+
+  $scope.pageLoads = 0;
+
+  $scope.getAllMessages = function($scope, $http) {
     //Allow cross site gets
     $http.defaults.useXDomain = true;
-    var url = baseUrl + '/maven12/rest/messages';
+    var url = baseUrl + '/rest/messages';
 
-    $http({method: 'GET', url: url})
-    .success(function(data, status, headers, config) {
-      $scope.messages = data;
-      $scope.error = '';
+    $http({
+      method: 'GET',
+      url: url
     })
-    .error(function(data, status, headers, config) {
-      $scope.error = 'Error: Status: ' + status;
-    });
+      .success(function(data, status, headers, config) {
+        $scope.messages = data;
+        $scope.error = '';
+      })
+      .error(function(data, status, headers, config) {
+        $scope.error = 'Error: Status: ' + status;
+      });
   }
 
   $scope.createMessage = function(newMessage) {
-    var createUrl = baseUrl + '/maven12/rest/message/create/';
+    var createUrl = baseUrl + '/rest/message/create/';
     var combinedCreateUrl = createUrl + newMessage;
     $http({
       method: 'POST',
@@ -39,17 +45,17 @@ var MainController = function($scope, $http, $interval) {
       $scope.error = 'Error: Status: ' + status;
     });
   };
-  
-  $scope.refreshPage = function(){
+
+  $scope.refreshPage = function() {
     $scope.getAllMessages($scope, $http);
     $scope.pageLoads += 1;
   }
-  
+
 
   $scope.getAllMessages($scope, $http);
-  
+
   $interval($scope.refreshPage, 2000);
 
 }
 
-  app.controller("MainController", MainController);
+app.controller("MainController", MainController);
